ee565ec443f8b70c3e0c5e2e5b50c5437d9d5d06
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-14 23:37:09 -0700

    scaffolding app for app a day may 14

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DateTrackerScaffold::Application.config.secret_token = 'fb2250e5f342df7bad3cbc926194d77f57954ac18c3738229122f645979c16834aee0dd4d436c353b16bfc245e6798839508612b639360a1a3b55c27fab07f23'
